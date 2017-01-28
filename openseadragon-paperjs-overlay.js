6243708af02b5195c84e9b5a01d527ca1b45c763
TheGuardianWolf <lichking21st@windowslive.com>
2017-01-28 17:39:06 +1300

    Implement clear function. Fix indents.

--- openseadragon-paperjs-overlay.js
+++ openseadragon-paperjs-overlay.js
@@ -8,7 +8,7 @@
 
 	if (!paper) {
 		var paper;
-		
+
 		if (typeof module === 'object' && module.exports) {
 			paper = require('paper');
 		}
@@ -19,94 +19,94 @@
 			console.error('[openseadragon-canvas-overlay] requires PaperJS');
 			return;
 		}
-    }
+	}
 
-    if (!window.OpenSeadragon) {
-        console.error('[openseadragon-canvas-overlay] requires OpenSeadragon');
-        return;
-    }
+	if (!window.OpenSeadragon) {
+		console.error('[openseadragon-canvas-overlay] requires OpenSeadragon');
+		return;
+	}
 
 
-    // ----------
-    OpenSeadragon.Viewer.prototype.paperjsOverlay = function() {
-        if (this._paperjsOverlayInfo) {
-            return this._paperjsOverlayInfo;
-        }
+	// ----------
+	OpenSeadragon.Viewer.prototype.paperjsOverlay = function() {
+		if (this._paperjsOverlayInfo) {
+			return this._paperjsOverlayInfo;
+		}
 
-        this._paperjsOverlayInfo = new Overlay(this);
-        return this._paperjsOverlayInfo;
-    };
+		this._paperjsOverlayInfo = new Overlay(this);
+		return this._paperjsOverlayInfo;
+	};
 
-    // ----------
-    var Overlay = function(viewer) {
-        var self = this;
+	// ----------
+	var Overlay = function(viewer) {
+		var self = this;
 
-        this._viewer = viewer;
+		this._viewer = viewer;
 
-        this._containerWidth = 0;
-        this._containerHeight = 0;
+		this._containerWidth = 0;
+		this._containerHeight = 0;
 
-        this._canvasdiv = document.createElement( 'div');
-        this._canvasdiv.style.position = 'absolute';
-        this._canvasdiv.style.left = 0;
-        this._canvasdiv.style.top = 0;
-        this._canvasdiv.style.width = '100%';
-        this._canvasdiv.style.height = '100%';
-        this._viewer.canvas.appendChild(this._canvasdiv);
+		this._canvasdiv = document.createElement( 'div');
+		this._canvasdiv.style.position = 'absolute';
+		this._canvasdiv.style.left = 0;
+		this._canvasdiv.style.top = 0;
+		this._canvasdiv.style.width = '100%';
+		this._canvasdiv.style.height = '100%';
+		this._viewer.canvas.appendChild(this._canvasdiv);
 
-        this._canvas = document.createElement('canvas');
-        this._canvas.setAttribute('id', 'osd-overlaycanvas');
-        this._canvasdiv.appendChild(this._canvas);
-        this.resize();
+		this._canvas = document.createElement('canvas');
+		this._canvas.setAttribute('id', 'osd-overlaycanvas');
+		this._canvasdiv.appendChild(this._canvas);
+		this.resize();
 
-        paper.setup(this._canvas);
+		paper.setup(this._canvas);
 
-        this._viewer.addHandler('update-viewport', function() {
-            self.resize();
-            self.resizecanvas();
-        });
+		this._viewer.addHandler('update-viewport', function() {
+			self.resize();
+			self.resizecanvas();
+		});
 
-        this._viewer.addHandler('open', function() {
-            self.resize();
-            self.resizecanvas();
-        });
+		this._viewer.addHandler('open', function() {
+			self.resize();
+			self.resizecanvas();
+		});
 
-        this.resize();
-    };
+		this.resize();
+	};
 
-    // ----------
-    Overlay.prototype = {
-        // ----------
-        paperCanvas: function() {
-            return this._canvas;
-        },
-        clear: function() {
-           // TODO: check what needs to be added here
-        },
-        // ----------
-        resize: function() {
-            if (this._containerWidth !== this._viewer.container.clientWidth) {
-                this._containerWidth = this._viewer.container.clientWidth;
-                this._canvasdiv.setAttribute('width', this._containerWidth);
-                this._canvas.setAttribute('width', this._containerWidth);
-            }
-            if (this._containerHeight !== this._viewer.container.clientHeight) {
-                this._containerHeight = this._viewer.container.clientHeight;
-                this._canvasdiv.setAttribute('height', this._containerHeight);
-                this._canvas.setAttribute('height', this._containerHeight);
-            }
-        },
-        resizecanvas: function() {
-                this._canvasdiv.setAttribute('width', this._containerWidth);
-                this._canvas.setAttribute('width', this._containerWidth);
-                this._canvasdiv.setAttribute('height', this._containerHeight);
-                this._canvas.setAttribute('height', this._containerHeight);
-                paper.view.viewSize = new paper.Size(this._containerWidth, this._containerHeight);
-                var viewportZoom = this._viewer.viewport.getZoom(true);
-                var image1 = this._viewer.world.getItemAt(0);
-                paper.view.zoom = image1.viewportToImageZoom(viewportZoom);
-                var center = this._viewer.viewport.viewportToImageCoordinates(this._viewer.viewport.getCenter(true));
-                paper.view.center = new paper.Point(center.x, center.y);
-       }
-    };
+	// ----------
+	Overlay.prototype = {
+		// ----------
+		paperCanvas: function() {
+			return this._canvas;
+		},
+		clear: function() {
+			paper.project.clear()
+		},
+		// ----------
+		resize: function() {
+			if (this._containerWidth !== this._viewer.container.clientWidth) {
+				this._containerWidth = this._viewer.container.clientWidth;
+				this._canvasdiv.setAttribute('width', this._containerWidth);
+				this._canvas.setAttribute('width', this._containerWidth);
+			}
+			if (this._containerHeight !== this._viewer.container.clientHeight) {
+				this._containerHeight = this._viewer.container.clientHeight;
+				this._canvasdiv.setAttribute('height', this._containerHeight);
+				this._canvas.setAttribute('height', this._containerHeight);
+			}
+		},
+		resizecanvas: function() {
+			this._canvasdiv.setAttribute('width', this._containerWidth);
+			this._canvas.setAttribute('width', this._containerWidth);
+			this._canvasdiv.setAttribute('height', this._containerHeight);
+			this._canvas.setAttribute('height', this._containerHeight);
+			paper.view.viewSize = new paper.Size(this._containerWidth, this._containerHeight);
+			var viewportZoom = this._viewer.viewport.getZoom(true);
+			var image1 = this._viewer.world.getItemAt(0);
+			paper.view.zoom = image1.viewportToImageZoom(viewportZoom);
+			var center = this._viewer.viewport.viewportToImageCoordinates(this._viewer.viewport.getCenter(true));
+			paper.view.center = new paper.Point(center.x, center.y);
+		}
+	};
 })();
